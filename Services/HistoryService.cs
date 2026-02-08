5ff0d90c6efa268d64fce2dafe8577759fb1bd45
Ваше Имя <.email@example.com>
2026-02-08 13:42:35 +1000

    Добавить relayCommand

---
+++ Services/HistoryService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace GraphicsEditor.Services
+{
+
+    public class HistoryService
+    {
+        private readonly Stack<RenderTargetBitmap> _history = new Stack<RenderTargetBitmap>();
+        private const int MaxHistorySize = 20;
+
+        public void SaveState(RenderTargetBitmap bitmap)
+        {
+            _history.Push(bitmap);
+
+            if (_history.Count > MaxHistorySize)
+            {
+                var temp = new Stack<RenderTargetBitmap>();
+                for (int i = 0; i < MaxHistorySize; i++)
+                {
+                    temp.Push(_history.Pop());
+                }
+                _history.Clear();
+                while (temp.Count > 0)
+                {
+                    _history.Push(temp.Pop());
+                }
+            }
+        }
+
+        public RenderTargetBitmap Undo()
+        {
+            if (_history.Count > 1)
+            {
+                _history.Pop(); 
+                return _history.Peek(); 
+            }
+            return null;
+        }
+
+        public bool CanUndo => _history.Count > 1;
+
+        public void Clear()
+        {
+            _history.Clear();
+        }
+    }
+}
